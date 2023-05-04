63dd3f11007f4d785430cf40b8f1ac58fb129114
Rokon Ron <rokon.raz@gmail.com>
2023-05-05 02:57:37 +0600

    Convert text to nato phonetic text using here comprehension

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ print(nato_name['A'])
 
 user_input = input("Write your massage: ").upper()
 
-empty_list = []
+# empty_list = []
+#
+# for char in user_input:
+#     if char in nato_name:
+#         empty_list.append(nato_name[char])
+#
+# output = ' '.join(empty_list)
+# print(output)
 
-for char in user_input:
-    if char in nato_name:
-        empty_list.append(nato_name[char])
+empty_list = [nato_name[char] for char in user_input if char in nato_name]
 
-output = ' '.join(empty_list)
-print(output)
+print(empty_list)
