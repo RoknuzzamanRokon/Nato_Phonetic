eeaa8612c09c1fbc5a4e8b5309a3e0580427daef
Rokon Ron <rokon.raz@gmail.com>
2023-05-05 17:27:46 +0600

    Try to change word if word < 5 then all upper

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ user_input = input("Write your massage: ").upper()
 # output = ' '.join(empty_list)
 # print(output)
 
-empty_list =' '.join( [nato_name[char] for char in user_input if char in nato_name])
-
+empty_list =' '.join([nato_name[char] for char in user_input if char in nato_name])
+new = []
+for i in empty_list:
+    print(i)
+#     if len(empty_list[i]) > 5:
+#         new.append(empty_list[i])
+# print(new)
 print(empty_list)
