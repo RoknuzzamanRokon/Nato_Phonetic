186c4e3b2e4303990c409063f82a99a54b1a8a18
Rokon Ron <rokon.raz@gmail.com>
2023-05-08 00:44:24 +0600

    Comprehension this code

--- main.py
+++ main.py
@@ -5,19 +5,22 @@ nato_name = nato_phonetic()
 
 user_input = input("Write your massage: ").upper()
 
-empty_list = []
+# empty_list = []
+empty_list = [nato_name[char] for char in user_input if char in nato_name]
 
-for char in user_input:
-    if char in nato_name:
-        empty_list.append(nato_name[char])
+# for char in user_input:
+#     if char in nato_name:
+#         empty_list.append(nato_name[char])
 
 output = ' '.join(empty_list)
 print(output)
 
 big_word = []
-for lan in empty_list:
-    if len(lan) > 5:
-        big_word.append(lan)
+big_word = [x for x in empty_list if len(x) > 5]
+
+# for lan in empty_list:
+#     if len(lan) > 5:
+#         big_word.append(lan)
 
 print(empty_list)
 print("\n")
