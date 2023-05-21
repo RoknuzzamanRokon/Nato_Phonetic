7a9f9e00bffee73cfa861a8781dc16bb77695e26
Rokon Ron <rokon.raz@gmail.com>
2023-05-21 11:32:21 +0600

    JCAE

--- main.py
+++ main.py
@@ -6,20 +6,8 @@ print(nato_name['A'])
 
 user_input = input("Write your massage: ").upper()
 
-# empty_list = []
-#
-# for char in user_input:
-#     if char in nato_name:
-#         empty_list.append(nato_name[char])
-#
-# output = ' '.join(empty_list)
-# print(output)
 
 empty_list =' '.join([nato_name[char] for char in user_input if char in nato_name])
 new = []
-for i in empty_list:
-    print(i)
-#     if len(empty_list[i]) > 5:
-#         new.append(empty_list[i])
-# print(new)
+
 print(empty_list)
