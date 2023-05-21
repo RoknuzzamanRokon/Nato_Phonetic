e14e301cc9890f0c596d255048111ebbf249b070
Rokon Ron <rokon.raz@gmail.com>
2023-05-21 17:09:49 +0600

    Create a generate_phonetic() function.

--- main_try_expet_exmple.py
+++ main_try_expet_exmple.py
@@ -2,7 +2,17 @@ from alphabet import nato_phonetic
 
 nato_name = nato_phonetic()
 
-user = input("Write a message: ")
 
-print(nato_name[f"{user}"])
+def generate_phonetic():
+    user = input("Write a message: ").upper()
+
+    try:
+        output_list = ' '.join([nato_name[num] for num in user])
+    except KeyError:
+        print("Sorry, You put a number,but here need a word or a sentence")
+    else:
+        print(output_list)
+
+
+generate_phonetic()
 
